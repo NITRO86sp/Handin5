72ce8a8bce1318ea0133c5203c01c9d2c730ab83
Stelios Papaoikonomou <spapaoikonomou@hotmail.com>
2015-09-30 11:31:12 +0200

    fixed augment!

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -3,9 +3,10 @@ public class Algorithm {
     public int augment(int flow, Edge[] P){
         int b=bottleneck(P,flow);
         for (Edge e : P){
-            if (e.isForward()) {
+            if (e.isForward())
                e.addFlow(b);
-            }
+            else
+                e.addFlow(-b);
 
         }
         return flow;
