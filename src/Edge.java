10b6423eca3ff28e449352d2f6e4e6402e145328
Stelios Papaoikonomou <spapaoikonomou@hotmail.com>
2015-09-30 09:17:40 +0200

    Edge helper functions

--- src/Edge.java
+++ src/Edge.java
@@ -1,14 +1,42 @@
 
 public class Edge {
     Node a, b;
-    int Capacity, current, rest;
+    int capacity, current, rest;
 
 
     public Edge(Node a, Node b, int C){
         this.a=a;
         this.b=b;
-        Capacity=C;
+        capacity=C;
         current=0;
         rest=0;
+    }
+    
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public int getCurrent(){
+        return current;
+    }
+
+    public int getRest(){
+        return rest;
+    }
+
+    public boolean canAdd(int diff){
+        if (current+diff>capacity || current+diff<0)
+            return false;
+        return true;
+    }
+
+    public boolean addFlow(int diff){
+        if (current+diff>capacity || current+diff<0)
+            return false;
+        else{
+            current+=diff;
+            rest-=diff;
+            return true;
+        }
     }
 }
